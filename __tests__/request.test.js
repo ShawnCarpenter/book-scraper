661bc85bc2db708148eea2a7f052094b9224186e
Shawn Carpenter <shawn.carpeenter@gmail.com>
2020-09-15 17:26:39 -0700

    lab done

--- __tests__/request.test.js
+++ __tests__/request.test.js
@@ -1,8 +1,8 @@
-const request = require('../lib/request');
+const request = require('../lib/scraper/request');
 
 describe('request function', () => {
   it('makes a request to the books.toscrape.com and returns html', async() => {
-    const document = await request();
+    const document = await request(1);
     expect(document.querySelector('.col-sm-8 > a').textContent).toEqual('Books to Scrape');
   });
 }); 
